1f0c8d21ce770af674c3678811bc1f04cc478221
atchyutn <atchyutnagabhairava@gmail.com>
2017-04-04 00:09:43 +0530

    Solved Day-1 problem in ruby

---
+++ Day 1: Data Types/solution.rb
@@ -0,0 +1,13 @@
+
+    # Declare second integer, double, and String variables.
+    mi = gets
+    md = gets
+    ms = gets
+    # Read and save an integer, double, and String to your variables.
+    # Print the sum of both integer variables on a new line.
+    puts i.to_i + mi.to_i
+    # Print the sum of the double variables on a new line.
+    puts (d.to_f + md.to_f).round(1)
+    # Concatenate and print the String variables on a new line
+    # The 's' variable above should be printed first.
+    puts s.to_s + ms.to_s
