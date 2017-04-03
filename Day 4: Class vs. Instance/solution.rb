70750de6599ab33a0219880deaa662ba3254be38
atchyutn <atchyutnagabhairava@gmail.com>
2017-04-03 23:50:40 +0530

    Solved Day-4 problem in ruby

---
+++ Day 4: Class vs. Instance/solution.rb
@@ -0,0 +1,12 @@
+class Person
+    attr_accessor :age
+    def initialize(initialAge)
+        # Add some more code to run some checks on initialAge
+    end
+    def amIOld()
+      # Do some computations in here and print out the correct statement to the console
+    end
+    def yearPasses()
+      # Increment the age of the person in here
+    end
+end
